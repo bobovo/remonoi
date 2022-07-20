7510349118c82ea4db861b91d5e6966209c55d39
bobkali <bobovo@gmail.com>
2022-07-19 20:16:50 -0400

    fix btn-hide-show.js all buttons ok

--- js/btn-hide-show.js
+++ js/btn-hide-show.js
@@ -1,11 +1,47 @@
 window.onload = function () {
     
+    document.getElementById("btn11").onclick = function (){
+    hide();
+    }
+    document.getElementById("btn12").onclick = function (){
+    show();
+    }
+    
     document.getElementById("btn21").onclick = function (){
     hide();
     }
     document.getElementById("btn22").onclick = function (){
     show();
     }
+    
+    document.getElementById("btn31").onclick = function (){
+    hide();
+    }
+    document.getElementById("btn32").onclick = function (){
+    show();
+    }
+    
+    document.getElementById("btn41").onclick = function (){
+    hide();
+    }
+    document.getElementById("btn42").onclick = function (){
+    show();
+    }
+    
+    document.getElementById("btn51").onclick = function (){
+    hide();
+    }
+    document.getElementById("btn52").onclick = function (){
+    show();
+    }
+    
+    document.getElementById("btn61").onclick = function (){
+    hide();
+    }
+    document.getElementById("btn62").onclick = function (){
+    show();
+    }
+    
 }
 
 function hide() {
